254c8da80b4c60ab67f4caadeb035211c80d6ec7
Christopher Wilhelm H. <info@kitsoft.at>
2024-02-09 23:44:41 +0100

    Added Playerdata to int. API

---
+++ src/main/java/eu/lotusgaming/mc/misc/Playerdata.java
@@ -0,0 +1,33 @@
+//Created by Chris Wille at 09.02.2024
+package eu.lotusgaming.mc.misc;
+
+public enum Playerdata {
+	MinecraftUUID("mcuuid"),
+	LotusHardID("id"),
+	LotusChangeID("lgcid"),
+	Clan("clan"),
+	Name("name"),
+	Nick("nick"),
+	FirstJoin("firstJoin"),
+	LastJoin("lastJoin"),
+	CurrentLastServer("currentLastServer"),
+	Playtime("playTime"),
+	SideboardState("scoreboardState"),
+	isOnline("isOnline"),
+	isStaff("isStaff"),
+	MoneyBank("money_bank"),
+	MoneyPocket("money_pocket"),
+	MoneyInterestLevel("money_interestLevel"),
+	Language("language");
+	
+	public String playerData;
+	
+	Playerdata(String playerData) {
+		this.playerData = playerData;
+	}
+	
+	public String getColumnName() {
+		return playerData;
+	}
+}
+
