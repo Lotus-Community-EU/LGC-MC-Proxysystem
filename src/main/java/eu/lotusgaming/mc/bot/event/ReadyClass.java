decf5b273d00994fb29e328cdc7c5212c1a061e7
Christopher Wilhelm H. <info@kitsoft.at>
2024-02-09 19:31:20 +0100

    Added ReadyClass for Discord Bot Part.

---
+++ src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
@@ -0,0 +1,15 @@
+//Created by Chris Wille at 09.02.2024
+package eu.lotusgaming.mc.bot.event;
+
+import eu.lotusgaming.mc.main.Main;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ReadyClass extends ListenerAdapter{
+	
+	public void onReady(ReadyEvent event) {
+		Main.logger.info("Bot has started as " + event.getJDA().getSelfUser().getEffectiveName() + " on " + event.getJDA().getGuilds().size() + " Guilds.");
+	}
+
+}
+
