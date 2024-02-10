61b459054cf2a15346829ed2ea1a39af9814f768
Christopher Wilhelm H. <info@kitsoft.at>
2024-02-10 01:25:05 +0100

    Added Join/Switch/Leave for DCB.

--- src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
+++ src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
@@ -1,6 +1,7 @@
 //Created by Chris Wille at 09.02.2024
 package eu.lotusgaming.mc.bot.event;
 
+import eu.lotusgaming.mc.game.event.UserLogEventsDCB;
 import eu.lotusgaming.mc.main.Main;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -10,6 +11,7 @@ public class ReadyClass extends ListenerAdapter{
 	public void onReady(ReadyEvent event) {
 		Main.logger.info("Bot has started as " + event.getJDA().getSelfUser().getEffectiveName() + " on " + event.getJDA().getGuilds().size() + " Guilds.");
 		Main.main.getProxy().getPluginManager().registerListener(Main.main, new ChatBridgeToDiscord(event.getJDA()));
+		Main.main.getProxy().getPluginManager().registerListener(Main.main, new UserLogEventsDCB(event.getJDA()));
 	}
 
 }
