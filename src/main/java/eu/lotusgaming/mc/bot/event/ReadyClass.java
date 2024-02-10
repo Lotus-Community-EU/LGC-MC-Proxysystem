544d8ce4df1fda76b2f4cb74eacdbbe748f24d61
Christopher Wilhelm H. <info@kitsoft.at>
2024-02-10 01:25:56 +0100

    Registered the additional Info Chat Bridge receiver.

--- src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
+++ src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
@@ -1,6 +1,7 @@
 //Created by Chris Wille at 09.02.2024
 package eu.lotusgaming.mc.bot.event;
 
+import eu.lotusgaming.mc.game.event.ChatBridgeInfoReceiver;
 import eu.lotusgaming.mc.game.event.UserLogEventsDCB;
 import eu.lotusgaming.mc.main.Main;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
@@ -12,6 +13,7 @@ public class ReadyClass extends ListenerAdapter{
 		Main.logger.info("Bot has started as " + event.getJDA().getSelfUser().getEffectiveName() + " on " + event.getJDA().getGuilds().size() + " Guilds.");
 		Main.main.getProxy().getPluginManager().registerListener(Main.main, new ChatBridgeToDiscord(event.getJDA()));
 		Main.main.getProxy().getPluginManager().registerListener(Main.main, new UserLogEventsDCB(event.getJDA()));
+		Main.main.getProxy().getPluginManager().registerListener(Main.main, new ChatBridgeInfoReceiver(event.getJDA()));
 	}
 
 }
