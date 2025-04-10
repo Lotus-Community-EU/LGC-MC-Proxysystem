ff5e75865955e7e1c429747e6cc0429b6081fe9f
Maurice H. <development@lotuscommunity.eu>
2025-04-10 13:37:00 +0200

    Added JDA Maintenance Command (whoopsie)

--- src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
+++ src/main/java/eu/lotusgaming/mc/bot/event/ReadyClass.java
@@ -1,9 +1,8 @@
 //Created by Chris Wille at 09.02.2024
 package eu.lotusgaming.mc.bot.event;
 
-import eu.lotusgaming.mc.bot.command.MC_Verify;
-import eu.lotusgaming.mc.game.event.ChatBridgeInfoReceiver;
-import eu.lotusgaming.mc.game.event.UserLogEventsDCB;
+import eu.lotusgaming.mc.bot.command.MaintenanceCommand;
+import eu.lotusgaming.mc.main.LotusManager;
 import eu.lotusgaming.mc.main.Main;
 import eu.lotusgaming.mc.misc.CommandAdder;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
@@ -14,11 +13,9 @@ public class ReadyClass extends ListenerAdapter{
 	public void onReady(ReadyEvent event) {
 		Main.logger.info("Bot has started as " + event.getJDA().getSelfUser().getEffectiveName() + " on " + event.getJDA().getGuilds().size() + " Guilds.");
 		CommandAdder.addCommands(event.getJDA());
-		Main.main.getProxy().getPluginManager().registerListener(Main.main, new ChatBridgeToDiscord(event.getJDA()));
-		Main.main.getProxy().getPluginManager().registerListener(Main.main, new UserLogEventsDCB(event.getJDA()));
-		Main.main.getProxy().getPluginManager().registerListener(Main.main, new ChatBridgeInfoReceiver(event.getJDA()));
-		event.getJDA().addEventListener(new MC_Verify(event.getJDA()));
 		event.getJDA().addEventListener(new ChatReportContextHandler());
+		event.getJDA().addEventListener(new MaintenanceCommand());
+		new LotusManager().init(event.getJDA());
 	}
 
 }
