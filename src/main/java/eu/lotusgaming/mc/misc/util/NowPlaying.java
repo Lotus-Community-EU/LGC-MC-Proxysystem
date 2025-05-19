4cb7646c78e11d2083e5b636c56243fa44ea5bce
Maurice H. <development@lotuscommunity.eu>
2025-05-19 18:46:38 +0200

    Added "localTrack" to check if a track is provided on Spotify or not.

--- src/main/java/eu/lotusgaming/mc/misc/util/NowPlaying.java
+++ src/main/java/eu/lotusgaming/mc/misc/util/NowPlaying.java
@@ -4,13 +4,14 @@ package eu.lotusgaming.mc.misc.util;
 public class NowPlaying {
 	
 	private final String track, artist, trackId;
-	private final boolean isPlaying;
+	private final boolean isPlaying, isLocalTrack;
 	
-	public NowPlaying(String track, String artist, boolean isPlaying, String trackId) {
+	public NowPlaying(String track, String artist, boolean isPlaying, String trackId, boolean isLocalTrack) {
 		this.track = track;
         this.artist = artist;
         this.isPlaying = isPlaying;
         this.trackId = trackId;
+		this.isLocalTrack = isLocalTrack;
 	}
 	
 	public String getArtist() {
@@ -27,5 +28,9 @@ public class NowPlaying {
 	
 	public boolean isPlaying() {
 		return this.isPlaying;
+	}
+
+	public boolean isLocalTrack() {
+		return this.isLocalTrack;
 	}
 }
